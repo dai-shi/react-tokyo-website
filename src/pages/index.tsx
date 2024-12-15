1a3b62fe3dcdc357bef3c636895faf8e32355658
tokutsu <tokutsu.beeworks@gmail.com>
2024-12-16 06:56:56 +0900

    remove prop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { JoinDiscordServer } from '../components/joinDiscordServer';
 export default async function HomePage() {
   return (
     <div>
-      <Head title='React Tokyo' />
+      <Head />
       <div className="grid grid-rows-3 lg:grid-cols-3 lg:grid-flow-row gap-y-2 gap-x-8">
         <div className="lg:col-span-2 flex flex-col items-start justify-center">
           <h1 className="text-4xl font-bold tracking-tight">We are React Tokyo Community</h1>
