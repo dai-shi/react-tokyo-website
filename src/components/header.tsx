dab080651c7d294d111de274cf7997ef3b4b9cfa
Takato Nakamura <takato.nakamura@access-company.com>
2024-12-16 22:42:52 +0900

    add button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
 import { Link } from 'waku';
+import { JoinDiscordServer } from './joinDiscordServer';
 
 export const Header = () => {
   return (
-    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
-      <h2 className="text-2xl font-bold tracking-tight">
+    <header className="flex items-center gap-4 p-6 w-full lg:fixed lg:left-0 lg:top-0">
+      <h2 className="text-2xl font-bold tracking-tight flex-grow">
         <Link to="/">React Tokyo</Link>
       </h2>
+      <div className="ml-auto">
+        <JoinDiscordServer />
+      </div>
     </header>
   );
 };
