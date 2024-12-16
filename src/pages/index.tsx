eb5e835050c51574c12d74a1761520f92434d3b8
Qian Yu <tubring25@gmail.com>
2024-12-16 21:05:46 +0900

    docs: replace catchphrase and update description text (#13)
    * docs: replace catchphrase and update description text
    
    * style:adjust logo layout for mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,32 @@ const meta = {
   title: 'React Tokyo',
 };
 
+const descriptions = [
+  '東京を中心に、オンラインとオフラインの両方で活動するReactコミュニティです。',
+  'オンラインでは、Discordを通じた情報交換や交流の場を提供し、オフラインではReactに関するイベントを開催します。',
+  '初心者から経験者まで、職種や技術力を問わず、誰でも気軽に参加できるのが特徴です。',
+  'Reactを学びたい人、新しいつながりを作りたい人、初めてコミュニティに参加する人も大歓迎です！',
+  '一緒にReactを楽しみましょう！',
+];
+
 export default async function HomePage() {
   return (
     <div>
       <title>{meta.title}</title>
-      <div className="grid grid-rows-3 lg:grid-cols-3 lg:grid-flow-row gap-y-2 gap-x-8">
-        <div className="lg:col-span-2 flex flex-col items-start justify-center">
-          <h1 className="text-4xl font-bold tracking-tight">We are React Tokyo Community</h1>
-        </div>
-        <div className="lg:row-span-3 flex items-center justify-center">
+      <div className="grid grid-rows-[auto_auto_auto] lg:grid-cols-3 lg:grid-flow-row gap-y-2 gap-x-8">
+        <div className="order-1 lg:order-2 lg:row-span-3 flex items-center justify-center">
           <div className="w-40 lg:w-80 rounded-full bg-[#F45554]">
             <img src="/images/react-tokyo-logo.png" alt="react tokyo logo" />
           </div>
         </div>
-        <div className="lg:row-span-2 lg:col-span-2 flex flex-col items-start justify-center gap-8">
+        <div className="order-2 lg:order-1 lg:col-span-2 flex flex-col items-start justify-center py-6">
+          <h1 className="text-4xl font-bold tracking-tight">分からないを分かち合う、広げようReactの世界。</h1>
+        </div>
+        <div className="order-3 lg:row-span-2 lg:col-span-2 flex flex-col items-start justify-center gap-8">
           <div>
-            <p>東京を中心としたReactのオンライン＆オフラインコミュニティです。</p>
-            <p>
-              オンラインでは、Discordを交流の場として活動し、オフラインでは、Reactのイベントを開催します。
-            </p>
+            {descriptions.map((description, index) => (
+              <p key={index}>{description}</p>
+            ))}
           </div>
           <div>
             <JoinDiscordServer />
