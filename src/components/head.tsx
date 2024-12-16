6ef0cc9a708842a6e1d16b55b5a25240b4678355
Hikaru Yoshino <osushicrusher@gmail.com>
2024-12-16 22:44:11 +0900

    Merge branch 'main' into feature/add-faq/yoshi2no

---
+++ src/components/head.tsx
@@ -0,0 +1,28 @@
+type MetaData = {
+  title: string;
+  description: string;
+  icon: string;
+};
+
+const DEFAULT_META_DATA: MetaData = {
+  title: "React Tokyo",
+  description: "A React community in Tokyo",
+  icon: "/images/favicon.png",
+};
+
+/**
+ * ページのメタデータ設定用のコンポーネント
+ */
+export const Head = (metaProps: Partial<MetaData>) => {
+  const meta: MetaData = {
+    ...DEFAULT_META_DATA,
+    ...metaProps,
+  };
+  return (
+    <>
+      <title>{meta.title}</title>
+      <meta name="description" content={meta.description} />
+      <link rel="icon" type="image/png" href={meta.icon} />
+    </>
+  );
+};
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -7,18 +7,9 @@ import { Footer } from '../components/footer';
 
 type RootLayoutProps = { children: ReactNode };
 
-const meta = {
-  title: 'React Tokyo',
-  description: 'A React community in Tokyo',
-  icon: '/images/favicon.png',
-};
-
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <div className="font-['Nunito']">
-      <title>{meta.title}</title>
-      <meta name="description" content={meta.description} />
-      <link rel="icon" type="image/png" href={meta.icon} />
       <Header />
       <main className="m-6 flex justify-center *:min-h-64 *:min-w-64 lg:m-0 lg:min-h-svh lg:pb-20">
         {children}
