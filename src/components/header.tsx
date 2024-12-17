dd66e6e97fe431286bb1a9f2fe3cf699f9cbb8a8
Takato Nakamura <takato.nakamura@access-company.com>
2024-12-17 18:24:58 +0900

    Refactor header component layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,11 @@ import { JoinDiscordServer } from './joinDiscordServer';
 
 export const Header = () => {
   return (
-    <header className="flex items-center gap-4 p-6 w-full lg:fixed lg:left-0 lg:top-0">
-      <h2 className="text-2xl font-bold tracking-tight flex-grow">
+    <header className="flex items-center p-6 justify-between w-full lg:fixed lg:left-0 lg:top-0">
+      <h2 className="text-2xl font-bold tracking-tight">
         <Link to="/">React Tokyo</Link>
       </h2>
-      <div className="ml-auto">
+      <div>
         <JoinDiscordServer />
       </div>
     </header>
