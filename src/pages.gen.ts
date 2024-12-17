e7c6e9f5a09466ffe60344689d3108d409aeb52d
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-12-18 00:06:15 +0900

    修正

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -2,9 +2,7 @@ import type { PathsForPages, GetConfigResponse } from "waku/router";
 
 import type { getConfig as Index_getConfig } from "./pages/index";
 
-type Page = {
-  DO_NOT_USE_pages: { path: "/" } & GetConfigResponse<typeof Index_getConfig>;
-};
+type Page = { path: "/" } & GetConfigResponse<typeof Index_getConfig>;
 
 declare module "waku/router" {
   interface RouteConfig {
