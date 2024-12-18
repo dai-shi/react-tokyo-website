75b707d34491ece3c39ef4718329d50559c11256
Shun Nakano <54790810+hakushun@users.noreply.github.com>
2024-12-19 00:50:07 +0900

    #15 Google Analytics導入 (#19)
    * #15 chore: add google analytics
    
    * #15 fix: fix Hydration failed
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

---
+++ src/pages/_root.tsx
@@ -0,0 +1,35 @@
+const code = `
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-VQNGS1YC35");
+`;
+
+export default async function RootElement({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VQNGS1YC35"
+        ></script>
+      </head>
+      <body>
+        <script>{code}</script>
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export const getConfig = async () => {
+  return {
+    render: "static",
+  } as const;
+};
