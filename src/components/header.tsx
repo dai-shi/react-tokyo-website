a2e2680e0a0b01fdabbf0424f9bea4030e850206
qianyu <tubring25@gmail.com>
2024-12-18 09:51:12 +0900

    add frosted glass effect to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'waku';
 
 export const Header = () => {
   return (
-    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
+    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0 lg:w-full bg-white/80 backdrop-blur-md">
       <h2 className="text-2xl font-bold tracking-tight">
         <Link to="/">React Tokyo</Link>
       </h2>
