40e8c2a3153f73da7b0d4aa883c67ec9c0935a12
Qian Yu <tubring25@gmail.com>
2024-12-18 20:05:07 +0900

    style: add frosted glass effect to header (#18)
    * add frosted glass effect to header
    
    * style fix
    
    * revert the style add of gap-4 in header
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { JoinDiscordServer } from "./joinDiscordServer";
 
 export const Header = () => {
   return (
-    <header className="flex items-center p-6 justify-between w-full lg:fixed lg:left-0 lg:top-0">
+    <header className="flex items-center p-6 justify-between w-full lg:fixed lg:left-0 lg:top-0 bg-white/80 backdrop-blur-md">
       <h2 className="text-xl sm:text-2xl font-bold tracking-tight">
         <Link to="/">React Tokyo</Link>
       </h2>
