ddfe572ccf50da71358f8a619aecc240fd1b0328
hakushun <hakushun.pianist@gmail.com>
2024-12-18 20:50:57 +0900

    #15 fix: fix Hydration failed

--- src/pages/_root.tsx
+++ src/pages/_root.tsx
@@ -1,19 +1,29 @@
-export default async function RootElement({ children }: { children: React.ReactNode }) {
+const code = `
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-VQNGS1YC35");
+`;
+
+export default async function RootElement({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ja">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VQNGS1YC35"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-VQNGS1YC35');
-          `
-        }}>
-        </script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VQNGS1YC35"
+        ></script>
       </head>
-      <body>{children}</body>
+      <body>
+        <script>{code}</script>
+        {children}
+      </body>
     </html>
   );
 }
@@ -21,5 +31,5 @@ export default async function RootElement({ children }: { children: React.ReactN
 export const getConfig = async () => {
   return {
     render: "static",
-  };
+  } as const;
 };
