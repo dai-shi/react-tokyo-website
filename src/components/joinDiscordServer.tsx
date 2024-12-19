8ba3eda46926dcc5b1db565ddb2ca491edd11b27
satoshi.fukai <chemistreal2@gmail.com>
2024-12-19 13:04:06 +0000

    レスポンシブ表示の指定フラグを追加しました

--- src/components/joinDiscordServer.tsx
+++ src/components/joinDiscordServer.tsx
@@ -1,4 +1,10 @@
-export const JoinDiscordServer = () => (
+type JoinDiscordServerProps = {
+  isResponsive?: boolean;
+};
+
+export const JoinDiscordServer = ({
+  isResponsive = false,
+}: JoinDiscordServerProps) => (
   <a
     className="px-4 py-2 rounded-md bg-[#5865F2] flex items-center gap-4"
     href="https://discord.gg/5B9jYpABUy"
@@ -10,7 +16,15 @@ export const JoinDiscordServer = () => (
       src="/images/discord-logo-white.svg"
       alt="Discord logo"
     />
-    <span className="text-white hidden sm:block">Join Discord Server</span>
-    <span className="text-white block sm:hidden">Join Us</span>
+    <span
+      className={`text-white ${isResponsive ? `hidden sm:block` : `block`}`}
+    >
+      Join Discord Server
+    </span>
+    <span
+      className={`text-white ${isResponsive ? `block sm:hidden` : `hidden`}`}
+    >
+      Join Us
+    </span>
   </a>
 );
