545e3a1a3c0d8df0d1a618ed1a44dae8ff8b5af1
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2024-12-20 02:10:50 +0900

    feat: reset input data

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,15 +1,24 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter_UNSTABLE as useRouter } from "waku";
+
 export const Contact = () => {
+  const [pending, setPending] = useState(false);
+  const router = useRouter();
+
   return (
-    <section className="space-y-4 lg:space-y-8">
+    <section id="section" className="space-y-4 lg:space-y-8">
       <h2 className="text-3xl font-bold">Contact</h2>
       <form
         action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSdjJ7qNPsLRccNZ2khhCAltpCGdAnCjlb80GrxoIgSsElhS-w/formResponse"
+        name="contactForm"
+        id="contactForm"
         className="grid gap-8 max-w-[480px]"
         method="POST"
+        target="#contactForm"
         onSubmit={(e) => {
-          e.preventDefault();
+          e.currentTarget.reset();
         }}
       >
         <label htmlFor="name" className="block space-y-1">
@@ -30,6 +39,7 @@ export const Contact = () => {
             name="entry.19281496"
             id="email"
             className="bg-slate-200 w-full rounded-md leading-6 p-2"
+            required
           />
         </label>
         <label htmlFor="contents" className="block space-y-1">
@@ -39,15 +49,18 @@ export const Contact = () => {
             name="entry.1625767701"
             id="contents"
             className="bg-slate-200 w-full rounded-md leading-6 p-2 min-h-[250px]"
+            required
           ></textarea>
         </label>
         <button
           type="submit"
+          disabled={pending}
           className="w-[150px] bg-[#5865F2] text-white p-2 rounded-full"
         >
           送信
         </button>
       </form>
+      <iframe name="hidden_iframe" className="hidden"></iframe>
     </section>
   );
 };
