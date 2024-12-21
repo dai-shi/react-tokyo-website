2efb591f8e7d4b89e80754314a70394b41af3189
spinrock <chemistreal2@gmail.com>
2024-12-21 23:44:18 +0900

    #22 Discordボタンのテキスト表示のレスポンシブ対応 (#22)
    * viewport meta tagの設定
    
    * Discordボタンのテキスト表示のレスポンシブ対応を実施
    
    * レスポンシブ表示の指定フラグを追加しました
    
    * フラグがtrueの時のみボタンサイズを1段階小さくしました
    
    * 判定フラグ名を変更しました: isResponsive → isHeader
    
    * [レビュー修正提案取込]不要なテンプレートリテラルの削除
    
    Co-authored-by: Teruhisa Yamamoto <t.yamamoto28@gmail.com>
    
    * clsxの導入
    
    * classNameの切り替えにclsxを適用
    
    * fix:formatの実行
    
    ---------
    
    Co-authored-by: Teruhisa Yamamoto <t.yamamoto28@gmail.com>
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- src/components/joinDiscordServer.tsx
+++ src/components/joinDiscordServer.tsx
@@ -1,4 +1,12 @@
-export const JoinDiscordServer = () => (
+import { clsx } from "clsx";
+
+type JoinDiscordServerProps = {
+  isHeader?: boolean;
+};
+
+export const JoinDiscordServer = ({
+  isHeader = false,
+}: JoinDiscordServerProps) => (
   <a
     className="px-4 py-2 rounded-md bg-[#5865F2] flex items-center gap-4"
     href="https://discord.gg/5B9jYpABUy"
@@ -6,10 +14,28 @@ export const JoinDiscordServer = () => (
     rel="noreferrer"
   >
     <img
-      className="h-5"
+      className={clsx({
+        "h-4": isHeader,
+        "h-5": !isHeader,
+      })}
       src="/images/discord-logo-white.svg"
       alt="Discord logo"
     />
-    <span className="text-white">Join Discord Server</span>
+    <span
+      className={clsx("text-white", {
+        "text-sm hidden sm:block": isHeader,
+        block: !isHeader,
+      })}
+    >
+      Join Discord Server
+    </span>
+    <span
+      className={clsx("text-white", {
+        "text-sm block sm:hidden": isHeader,
+        hidden: !isHeader,
+      })}
+    >
+      Join Us
+    </span>
   </a>
 );
