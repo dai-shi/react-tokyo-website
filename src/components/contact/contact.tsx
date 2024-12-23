1e9ec1eafb54ce7bbcc71819ee227015e3113a12
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2024-12-23 12:27:28 +0900

    fix: 問い合わせ内容の送信ロジックを修正

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,31 +1,17 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter_UNSTABLE as useRouter } from "waku";
+import { postContent } from "./postContent";
 
 export const Contact = () => {
-  const [pending, setPending] = useState(false);
-  const router = useRouter();
-
   return (
     <section id="section" className="space-y-4 lg:space-y-8">
       <h2 className="text-3xl font-bold">Contact</h2>
-      <form
-        action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSdjJ7qNPsLRccNZ2khhCAltpCGdAnCjlb80GrxoIgSsElhS-w/formResponse"
-        name="contactForm"
-        id="contactForm"
-        className="grid gap-8 max-w-[480px]"
-        method="POST"
-        target="#contactForm"
-        onSubmit={(e) => {
-          e.currentTarget.reset();
-        }}
-      >
+      <form action={postContent} className="grid gap-7 max-w-[480px] relative">
         <label htmlFor="name" className="block space-y-1">
           お名前<span className="text-[#F45554] text-sm ml-2">*必須</span>
           <input
             type="text"
-            name="entry.876026217"
+            name="name"
             id="name"
             className="bg-slate-200 w-full rounded-md leading-6 p-2"
             required
@@ -36,7 +22,7 @@ export const Contact = () => {
           <span className="text-[#F45554] text-sm ml-2">*必須</span>
           <input
             type="email"
-            name="entry.19281496"
+            name="email"
             id="email"
             className="bg-slate-200 w-full rounded-md leading-6 p-2"
             required
@@ -46,21 +32,18 @@ export const Contact = () => {
           お問い合わせ内容
           <span className="text-[#F45554] text-sm ml-2">*必須</span>
           <textarea
-            name="entry.1625767701"
-            id="contents"
+            name="contents"
             className="bg-slate-200 w-full rounded-md leading-6 p-2 min-h-[250px]"
             required
           ></textarea>
         </label>
         <button
           type="submit"
-          disabled={pending}
           className="w-[150px] bg-[#5865F2] text-white p-2 rounded-full"
         >
           送信
         </button>
       </form>
-      <iframe name="hidden_iframe" className="hidden"></iframe>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        "slide-in-left": "slide-in-left 0.5s ease",
+      },
+      keyframes: {
+        "slide-in-left": {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateX(0)",
+            opacity: "1",
+          },
+        },
+      },
+    },
+  },
 };
