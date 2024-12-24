2051a67e2eda814333e6ccaaf110d5c6898a170b
t6adev <t.yamamoto28@gmail.com>
2024-12-24 22:18:00 +0900

    add prettier settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,33 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import reactCompiler from "eslint-plugin-react-compiler";
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import prettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   ...tseslint.configs.recommended,
   {
-    files: ["**/*.{ts, tsx}"],
+    files: ['**/*.{ts, tsx}'],
     plugins: {
-      "react-compiler": reactCompiler,
+      'react-compiler': reactCompiler,
     },
     languageOptions: {
       globals: globals.browser,
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-      "react-compiler/react-compiler": "error",
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["warn"],
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-compiler/react-compiler': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
     },
   },
+  prettier,
 ];
