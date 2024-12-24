f6f16e4ec867c22d868a807c54662dab9d7532d6
t6adev <t.yamamoto28@gmail.com>
2024-12-24 22:59:32 +0900

    remove eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import reactCompiler from 'eslint-plugin-react-compiler';
-import prettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -29,5 +28,4 @@ export default [
       '@typescript-eslint/no-unused-vars': ['warn'],
     },
   },
-  prettier,
 ];
