f409ce66b1fe7474804bc87874c8305b90123794
Teruhisa Yamamoto <t.yamamoto28@gmail.com>
2024-12-24 23:38:23 +0900

    zenn記事のサムネイル画像を取得するスクリプトを作成 (#32)
    * add open-graph-scraper and tsx
    
    * add a script to generage blog list

---
+++ src/blogs.gen.ts
@@ -0,0 +1,14 @@
+export const GATHERED_BLOGS = [
+  {
+    url: 'https://zenn.dev/tell_y/articles/f013a03370ee27',
+    title: 'Re: React Tokyoというコミュニティを作ろうと思ったワケ',
+    imageUrl:
+      'https://res.cloudinary.com/zenn/image/upload/s--nN8lpuKi--/c_fit%2Cg_north_west%2Cl_text:notosansjp-medium.otf_55:Re%253A%2520React%2520Tokyo%25E3%2581%25A8%25E3%2581%2584%25E3%2581%2586%25E3%2582%25B3%25E3%2583%259F%25E3%2583%25A5%25E3%2583%258B%25E3%2583%2586%25E3%2582%25A3%25E3%2582%2592%25E4%25BD%259C%25E3%2582%258D%25E3%2581%2586%25E3%2581%25A8%25E6%2580%259D%25E3%2581%25A3%25E3%2581%259F%25E3%2583%25AF%25E3%2582%25B1%2Cw_1010%2Cx_90%2Cy_100/g_south_west%2Cl_text:notosansjp-medium.otf_37:Teruhisa%2520-%2520T6ADEV%2Cx_203%2Cy_121/g_south_west%2Ch_90%2Cl_fetch:aHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL3plbm4tdXNlci11cGxvYWQvYXZhdGFyL2VhNDg5NTAyZTkuanBlZw==%2Cr_max%2Cw_90%2Cx_87%2Cy_95/v1627283836/default/og-base-w1200-v2.png',
+  },
+  {
+    url: 'https://zenn.dev/dai_shi/articles/9f2760086fb31a',
+    title: 'React Tokyoというコミュニティを作ろうと思ったワケ',
+    imageUrl:
+      'https://res.cloudinary.com/zenn/image/upload/s--m1e0HcVL--/c_fit%2Cg_north_west%2Cl_text:notosansjp-medium.otf_55:React%2520Tokyo%25E3%2581%25A8%25E3%2581%2584%25E3%2581%2586%25E3%2582%25B3%25E3%2583%259F%25E3%2583%25A5%25E3%2583%258B%25E3%2583%2586%25E3%2582%25A3%25E3%2582%2592%25E4%25BD%259C%25E3%2582%258D%25E3%2581%2586%25E3%2581%25A8%25E6%2580%259D%25E3%2581%25A3%25E3%2581%259F%25E3%2583%25AF%25E3%2582%25B1%2Cw_1010%2Cx_90%2Cy_100/g_south_west%2Cl_text:notosansjp-medium.otf_37:daishi%2Cx_203%2Cy_121/g_south_west%2Ch_90%2Cl_fetch:aHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL3plbm4tdXNlci11cGxvYWQvYXZhdGFyLzcwZGNhM2E2Y2IuanBlZw==%2Cr_max%2Cw_90%2Cx_87%2Cy_95/v1627283836/default/og-base-w1200-v2.png',
+  },
+];
