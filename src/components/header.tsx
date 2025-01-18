0a6e3493c5f715cbd14b6c5589888013bf5cda51
miwashutaro0611 <jacksumiwa0061@gmail.com>
2025-01-18 17:18:21 +0900

    feat: about menu link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,19 @@ import { JoinDiscordServer } from './joinDiscordServer';
 
 export const Header = () => {
   return (
-    <header className="z-10 flex w-full items-center justify-between bg-white/80 p-6 backdrop-blur-md lg:fixed lg:left-0 lg:top-0">
-      <h2 className="text-xl font-bold tracking-tight sm:text-2xl">
-        <Link to="/">React Tokyo</Link>
-      </h2>
+    <header className="z-10 flex w-full items-start justify-between bg-white/80 p-6 backdrop-blur-md sm:items-center lg:fixed lg:left-0 lg:top-0">
+      <div className="flex flex-col gap-2 sm:flex-row sm:items-end sm:gap-4">
+        <h2 className="text-xl font-bold tracking-tight sm:text-2xl">
+          <Link to="/">React Tokyo</Link>
+        </h2>
+        <nav>
+          <ul className="flex gap-2">
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <div>
         <JoinDiscordServer isHeader />
       </div>
