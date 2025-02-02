d359528cb4c466e5e2121dde13aa2b8eaceb9d1e
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2025-02-02 13:57:16 +0900

    change: card style

--- src/components/members/card.tsx
+++ src/components/members/card.tsx
@@ -5,7 +5,7 @@ import { type Profile } from './memberProfiles';
  */
 export const Card = ({ profile }: { profile: Profile }) => {
   return (
-    <div className="grid w-full place-items-center gap-4 rounded-lg bg-stone-100 p-12 text-gray-700">
+    <div className="grid w-full place-items-center gap-4 rounded-lg bg-stone-100 p-12 text-gray-700 lg:p-8">
       <img
         className="h-32 rounded-full"
         src={profile.imageUrl}
