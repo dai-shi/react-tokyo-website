b4d74447665dc9475aa0713f2b73612010bdd21d
daishi <daishi@axlight.com>
2025-02-03 09:54:22 +0900

    update profile description

--- src/components/members/memberProfiles.ts
+++ src/components/members/memberProfiles.ts
@@ -11,7 +11,7 @@ export const INITIATOR_PROFILES: Profile[] = [
       'https://pbs.twimg.com/profile_images/1853633897558589440/Nuy9mou5_400x400.jpg',
     name: 'Daishi Kato',
     description:
-      'オープンソース開発者。Zustand🐻 Jotai👻 Valtio🧙 Waku⛩️ を開発。',
+      'オープンソース開発者。Reactのステート管理ライブラリ Zustand🐻 Jotai👻 Valtio🧙 が有名。近年はReactフレームワーク Waku⛩️ の開発に注力。React Tokyoの活動が盛り上がることに期待。',
     xUrl: 'https://x.com/dai_shi',
   },
   {
