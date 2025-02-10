3c59f9134b4dccaa4d12f344c7b2c599ba9fecd9
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2025-02-10 15:53:48 +0900

    add: profiles

--- src/components/members/memberProfiles.ts
+++ src/components/members/memberProfiles.ts
@@ -30,20 +30,20 @@ export const INITIATOR_PROFILES: Profile[] = [
 export const SUPPORTER_PROFILES: Profile[] = [
   {
     key: 0,
-    imageUrl: '/images/react-tokyo-logo.png',
-    name: '???',
-    description: 'Coming soon...',
+    imageUrl:
+      'https://pbs.twimg.com/profile_images/1883341436617441280/vaE-JwnH_400x400.jpg',
+    name: 'Yusuke Kikuta',
+    description:
+      'フリーランスフルスタックエンジニア。普段はAIエージェントの制作やweb3領域、Blockchainなどの開発を行なっております。Reactのコンポーネント指向が好きです！お仕事のご連絡いつでもお待ちしております！',
+    xUrl: 'https://x.com/yusuke_05092005',
   },
   {
     key: 1,
-    imageUrl: '/images/react-tokyo-logo.png',
-    name: '???',
-    description: 'Coming soon...',
-  },
-  {
-    key: 2,
-    imageUrl: '/images/react-tokyo-logo.png',
-    name: '???',
-    description: 'Coming soon...',
+    imageUrl:
+      'https://pbs.twimg.com/profile_images/1624662455682215936/d6fJLpgm_400x400.jpg',
+    name: 'ytaisei（たいせー）',
+    description:
+      'Webフロントエンドエンジニア。普段はtoB向けの生成AIアプリケーション開発に従事しています。Reactのエコシステムと設計を考えることが好きです！',
+    xUrl: 'https://x.com/ytaisei_',
   },
 ];
