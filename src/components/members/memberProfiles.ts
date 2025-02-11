a2ba7dac6904ab174b463903cdf0c1e03c4c1b17
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2025-02-12 00:20:07 +0900

    add: profiles

--- src/components/members/memberProfiles.ts
+++ src/components/members/memberProfiles.ts
@@ -51,7 +51,7 @@ export const COLLABORATOR_PROFILES: Profile[] = [
     key: 1,
     imageUrl:
       'https://pbs.twimg.com/profile_images/1624662455682215936/d6fJLpgm_400x400.jpg',
-    name: 'ytaisei（たいせー）',
+    name: 'ytaisei',
     description: [
       'Webフロントエンドエンジニア',
       '普段はtoB向けの生成AIアプリケーション開発に従事しています。Reactのエコシステムと設計を考えることが好きです！',
@@ -73,7 +73,7 @@ export const COLLABORATOR_PROFILES: Profile[] = [
     key: 3,
     imageUrl:
       'https://pbs.twimg.com/profile_images/1888927761454227456/BLz1aFZm_400x400.jpg',
-    name: 'タム@エンジニア',
+    name: 'tamu',
     description: [
       'Webエンジニア',
       'ToC向けのWebアプリをフロントエンドはNext.js、Reactを用いて開発しています。SESでエンジニアスタートして、今は自社開発の会社で働いています。Reactが好きな方、Next.jsが好きな方、フロントエンドが好きな方、',
@@ -93,5 +93,28 @@ export const COLLABORATOR_PROFILES: Profile[] = [
       'React.jsが好きな方、これから触っていこうと思っている方など、いろいろな方に参加していただきたいと思っています。',
     ],
     xUrl: 'https://x.com/jackmiwamiwa',
+  },
+  {
+    key: 5,
+    imageUrl:
+      'https://pbs.twimg.com/profile_images/1635878143579013120/EFedSC6O_400x400.jpg',
+    name: 'cordelia',
+    description: [
+      'Web Developer',
+      'サイト、アプリケーション、ライブラリなど様々な開発に興味あり！フロントエンドもバックエンドもデキる開発者を目指しています。React Tokyoでお会いしましょう👋',
+    ],
+    xUrl: 'https://x.com/cordelia_sixth',
+  },
+  {
+    key: 6,
+    imageUrl:
+      'https://pbs.twimg.com/profile_images/1018548906379001857/MlgEdhSP_400x400.jpg',
+    name: 'Ntaka',
+    description: [
+      'Webエンジニア',
+      '普段はサーバー・フロントエンド開発やクラウドアーキテクチャ設計など、幅広く取り組んでいます。',
+      'ReactTokyoでは、Reactを通じて多様なバックグラウンドを持つ方々と交流し、技術的な知見を深めていければと思っています！',
+    ],
+    xUrl: 'https://x.com/aTakatoNakamura',
   },
 ];
