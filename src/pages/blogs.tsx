8bdaa50a53a7935c991f1f75a59f066d519bbdf0
takaaki chida <takaaki.chida@findy.co.jp>
2025-03-02 10:47:20 +0900

    fix styles for blog page

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -5,7 +5,7 @@ export default async function BlogsPage() {
   return (
     <section className="text-gray-900 lg:pt-40">
       <Head title="Blogs - React Tokyo" />
-      <div className="mx-auto w-[calc(100vw-40px)] max-w-[1300px]">
+      <div className="mx-auto max-w-[1300px]">
         <div className="space-y-4 lg:space-y-8">
           <h1 className="text-3xl font-bold">BLOGS</h1>
           <div className="space-y-4 lg:text-lg">
