a794e42929df4fadef83d0469c95576fd89b07aa
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2025-03-08 18:20:00 +0900

    delete _root.tsx

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -9,7 +9,7 @@ type RootLayoutProps = { children: ReactNode };
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <div className="overflow-hidden font-['Nunito']">
+    <div className="overflow-hidden font-['Nunito'] has-[#sp-menu[data-expanded='true']]:overflow-hidden">
       <Header />
       <main className="m-6 flex justify-center *:min-h-64 *:min-w-64 lg:my-0 lg:min-h-svh lg:pb-20">
         {children}
